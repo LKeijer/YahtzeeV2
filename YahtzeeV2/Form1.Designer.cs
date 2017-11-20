96253276307e080b8ae5e90e54e9a92c32acdfab
Wafflebasket <Wafflebasket@outlook.com>
2017-11-20 15:03:32 +0100

    RANDOM CRASHES

--- YahtzeeV2/Form1.Designer.cs
+++ YahtzeeV2/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dice1 = new System.Windows.Forms.PictureBox();
+            this.dice2 = new System.Windows.Forms.PictureBox();
+            this.dice3 = new System.Windows.Forms.PictureBox();
+            this.dice4 = new System.Windows.Forms.PictureBox();
+            this.dice5 = new System.Windows.Forms.PictureBox();
+            this.rollDiceBtn = new System.Windows.Forms.Button();
+            this.displayResultsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dice1
+            // 
+            this.dice1.Image = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice1.InitialImage = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice1.Location = new System.Drawing.Point(89, 12);
+            this.dice1.Name = "dice1";
+            this.dice1.Size = new System.Drawing.Size(75, 75);
+            this.dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice1.TabIndex = 0;
+            this.dice1.TabStop = false;
+            // 
+            // dice2
+            // 
+            this.dice2.Image = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice2.InitialImage = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice2.Location = new System.Drawing.Point(170, 12);
+            this.dice2.Name = "dice2";
+            this.dice2.Size = new System.Drawing.Size(75, 75);
+            this.dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice2.TabIndex = 1;
+            this.dice2.TabStop = false;
+            // 
+            // dice3
+            // 
+            this.dice3.Image = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice3.InitialImage = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice3.Location = new System.Drawing.Point(251, 12);
+            this.dice3.Name = "dice3";
+            this.dice3.Size = new System.Drawing.Size(75, 75);
+            this.dice3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice3.TabIndex = 2;
+            this.dice3.TabStop = false;
+            // 
+            // dice4
+            // 
+            this.dice4.Image = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice4.InitialImage = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice4.Location = new System.Drawing.Point(332, 12);
+            this.dice4.Name = "dice4";
+            this.dice4.Size = new System.Drawing.Size(75, 75);
+            this.dice4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice4.TabIndex = 3;
+            this.dice4.TabStop = false;
+            // 
+            // dice5
+            // 
+            this.dice5.Image = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice5.InitialImage = global::YahtzeeV2.Properties.Resources.dice1_6;
+            this.dice5.Location = new System.Drawing.Point(413, 12);
+            this.dice5.Name = "dice5";
+            this.dice5.Size = new System.Drawing.Size(75, 75);
+            this.dice5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice5.TabIndex = 4;
+            this.dice5.TabStop = false;
+            // 
+            // rollDiceBtn
+            // 
+            this.rollDiceBtn.Location = new System.Drawing.Point(190, 128);
+            this.rollDiceBtn.Name = "rollDiceBtn";
+            this.rollDiceBtn.Size = new System.Drawing.Size(200, 75);
+            this.rollDiceBtn.TabIndex = 5;
+            this.rollDiceBtn.Text = "Roll this shiet";
+            this.rollDiceBtn.UseVisualStyleBackColor = true;
+            this.rollDiceBtn.Click += new System.EventHandler(this.rollDiceBtn_Click);
+            // 
+            // displayResultsLabel
+            // 
+            this.displayResultsLabel.AutoSize = true;
+            this.displayResultsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayResultsLabel.Location = new System.Drawing.Point(185, 225);
+            this.displayResultsLabel.Name = "displayResultsLabel";
+            this.displayResultsLabel.Size = new System.Drawing.Size(169, 29);
+            this.displayResultsLabel.TabIndex = 6;
+            this.displayResultsLabel.Text = "displayResults";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.displayResultsLabel);
+            this.Controls.Add(this.rollDiceBtn);
+            this.Controls.Add(this.dice5);
+            this.Controls.Add(this.dice4);
+            this.Controls.Add(this.dice3);
+            this.Controls.Add(this.dice2);
+            this.Controls.Add(this.dice1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Yahtzee";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox dice1;
+        private System.Windows.Forms.PictureBox dice2;
+        private System.Windows.Forms.PictureBox dice3;
+        private System.Windows.Forms.PictureBox dice4;
+        private System.Windows.Forms.PictureBox dice5;
+        private System.Windows.Forms.Button rollDiceBtn;
+        private System.Windows.Forms.Label displayResultsLabel;
     }
 }
 
--- YahtzeeV2/Form1.cs
+++ YahtzeeV2/Form1.cs
@@ -12,9 +12,128 @@ namespace YahtzeeV2
 {
     public partial class Form1 : Form
     {
+
+        //declarations
+        Image[] diceImg;
+        int[] diceValue;
+        int[] rollResults;
+        Random rngeesus;
+        //declarations
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                    // loading images into the image array
+            diceImg = new Image[8];
+            diceImg[0] = Properties.Resources.dice1_6;
+            diceImg[1] = Properties.Resources.dice1;
+            diceImg[2] = Properties.Resources.dice2;
+            diceImg[3] = Properties.Resources.dice3;
+            diceImg[4] = Properties.Resources.dice4;
+            diceImg[5] = Properties.Resources.dice5;
+            diceImg[6] = Properties.Resources.dice6;
+            diceImg[7] = Properties.Resources.rollDice;
+
+                    //setting up the integer array with no values
+            diceValue = new int[5] { 0, 0, 0, 0, 0 };
+                    
+            rngeesus = new Random();
+                    //setting up the integer array that will hold the results
+            rollResults = new int[6] { 0, 0, 0, 0, 0, 0 };
+        }
+
+        private void diceRoll()
+        {
+            for (int i = 0; i < diceValue.Length; i++)
+            {
+                diceValue[i] = rngeesus.Next(1, 7); // 7 not included, its 1,2,3,4,5,6.
+                dice1.Image = diceImg[diceValue[0]];
+                dice2.Image = diceImg[diceValue[1]];
+                dice3.Image = diceImg[diceValue[2]];
+                dice4.Image = diceImg[diceValue[3]];
+                dice5.Image = diceImg[diceValue[4]];
+
+                switch (diceValue[i])
+                {
+                    case 1:
+                        rollResults[0]++;
+                        break;
+                    case 2:
+                        rollResults[1]++;
+                        break;
+                    case 3:
+                        rollResults[2]++;
+                        break;
+                    case 4:
+                        rollResults[3]++;
+                        break;
+                    case 5:
+                        rollResults[4]++;
+                        break;
+                    case 6:
+                        rollResults[5]++;
+                        break;
+
+                }
+            }
+
+        }
+        
+        private void GetResult()
+        {
+            bool onePair, twoPair, threeOfKind, fullHouse, straight, highStraight, fourOfKind, fiveOfKind, six, five, four, three, two, one = false;
+            for (int i = 0; i < rollResults.Length; i ++)
+            {
+                if (rollResults[i] == 5)
+                    fiveOfKind = true;
+                else if (rollResults[i] == 4)
+                    fourOfKind = true;
+                else if (rollResults[i] == 3)
+                {
+                    threeOfKind = true;
+                    if (rollResults[i] == 2)
+                    {
+                        fullHouse = true;
+                        threeOfKind = false;
+
+                    }
+                }
+                else if (rollResults[0] == 1 && rollResults[1] == 1 && rollResults[2] == 1 && rollResults[3] == 1 && rollResults[4] == 1)
+                {
+                    straight = true;
+                }
+                else if (rollResults[5] == 1 && rollResults[1] == 1 && rollResults[2] == 1 && rollResults[3] == 1 && rollResults[4] == 1)
+                {
+                    highStraight = true;
+                }
+                else if (rollResults[i] == 2)
+                {
+                    onePair = true;
+                    for (int j = i+1; j < rollResults.Length; i++)
+                    {
+                        if (rollResults[j] == 2)
+                            twoPair = true;
+                    }
+                }
+
+
+            
+            }
+        }
+
+        private void ResultReset()
+        {
+
+        }
+
+        private void rollDiceBtn_Click(object sender, EventArgs e)
+        {
+            diceRoll();
+            GetResult();
         }
     }
 }
